bf653c51a092b6cb3c6ac8ab48530819abca95b6
Max <yozh.asmodeus@gmail.com>
2020-04-26 19:50:34 +0300

    Updates

---
+++ precache-manifest.ab9f2f616cb244ebfb9672ac87f2fa38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf70b6f1a9c84264379388a14727bdc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9baf57297a8c04a5b665",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "03a70bdd89d787918749",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9baf57297a8c04a5b665",
+    "url": "/static/js/2.9318f5b1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.9318f5b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03a70bdd89d787918749",
+    "url": "/static/js/main.d7818795.chunk.js"
+  },
+  {
+    "revision": "252f969779de0734efb3",
+    "url": "/static/js/runtime-main.75211130.js"
+  }
+]);
